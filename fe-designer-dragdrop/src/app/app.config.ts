13779e67192e41ecc2e8705dce06abf5adfac1ec
Elemer Zagoni <elemerz@gmail.com>
2025-10-22 17:41:44 +0300

    fix(startup): register A4_STYLES_PRELOAD_PROVIDER to populate style cache

--- fe-designer-dragdrop/src/app/app.config.ts
+++ fe-designer-dragdrop/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { A4_STYLES_PRELOAD_PROVIDER } from './core/startup/a4-styles.preload';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    A4_STYLES_PRELOAD_PROVIDER
   ]
 };
