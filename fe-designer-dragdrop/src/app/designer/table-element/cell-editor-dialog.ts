d1a7a7f9845d306f0ec430c5757802953089c89b
Elemer Zagoni <elemerz@gmail.com>
2025-10-23 17:47:12 +0300

    fix(quill): remove bad relative import and register formats on init

--- fe-designer-dragdrop/src/app/designer/table-element/cell-editor-dialog.ts
+++ fe-designer-dragdrop/src/app/designer/table-element/cell-editor-dialog.ts
@@ -1,6 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import Quill from 'quill';
-import '../../quill-setup';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { QuillModule } from 'ngx-quill';
@@ -31,6 +30,19 @@ export class CellEditorDialogComponent {
   };
 
   ngOnInit(): void {
+    // Dynamic registration (in case global setup not loaded)
+    try {
+      const Font = Quill.import('formats/font');
+      Font.whitelist = ['arial','helvetica','verdana','tahoma','trebuchet','times-new-roman','georgia','roboto','open-sans','lato','montserrat','poppins'];
+      Quill.register(Font, true);
+      const SizeStyle = Quill.import('attributors/style/size');
+      SizeStyle.whitelist = ['6pt','7pt','8pt','9pt','10pt','11pt','12pt','13pt','14pt','15pt','16pt','17pt','18pt','20pt','24pt','28pt','32pt'];
+      Quill.register(SizeStyle, true);
+      const Parchment: any = Quill.import('parchment');
+      const LineHeightStyle = new Parchment.Attributor.Style('lineheight','line-height',{ scope: Parchment.Scope.INLINE, whitelist: ['1','1.15','1.25','1.5','1.75','2','2.25','2.5'] });
+      Quill.register(LineHeightStyle, true);
+    } catch {}
+
     this.contentValue = this.initialContent && this.initialContent !== '&nbsp;' ? this.initialContent : '';
   }
 
