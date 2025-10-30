5fc0dde0c3964c591fa97064774a4f6a6ce4e737
Elemer Zagoni <elemerz@gmail.com>
2025-10-30 17:44:39 +0200

    chore(types): add qrcode-svg module declaration

---
+++ fe-designer-dragdrop/src/types/qrcode-svg.d.ts
@@ -0,0 +1,16 @@
+declare module 'qrcode-svg' {
+  type ErrorCorrectionLevel = 'L' | 'M' | 'Q' | 'H';
+  interface QRCodeOptions {
+    content: string;
+    padding?: number;
+    width?: number;
+    height?: number;
+    color?: string;
+    background?: string;
+    ecl?: ErrorCorrectionLevel;
+  }
+  export default class QRCode {
+    constructor(options: QRCodeOptions);
+    svg(): string;
+  }
+}
