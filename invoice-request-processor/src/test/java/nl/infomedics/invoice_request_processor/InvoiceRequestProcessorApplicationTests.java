187f449608f690eefce17f3b483f14e017b36a21
elemerz <elemerz@gmail.com>
2025-11-05 11:27:21 +0200

    Added new sub-app: invoice-request-processor.

---
+++ invoice-request-processor/src/test/java/nl/infomedics/invoice_request_processor/InvoiceRequestProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.infomedics.invoice_request_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceRequestProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
