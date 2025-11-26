6f1f22be1faeaa5b8e589e260aa9a60aa06ebec0
elemerz <elemerz@gmail.com>
2025-11-26 18:13:04 +0200

    Renamed SprinBootApp to PdfCreatorApplication

--- pdf-creator/src/main/java/nl/infomedics/xhtml2pdf/PdfCreatorApplication.java
+++ pdf-creator/src/main/java/nl/infomedics/xhtml2pdf/PdfCreatorApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"nl.infomedics.xhtml2pdf", "nl.infomedics.reporting"})
-public class Xhtml2PdfApplication {
+public class PdfCreatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Xhtml2PdfApplication.class, args);
+		// Silence OpenHTMLToPDF's JUL logging before it installs its own console handler
+		System.setProperty("xr.util-logging.loggingEnabled", "false");
+		SpringApplication.run(PdfCreatorApplication.class, args);
 	}
 
 }
